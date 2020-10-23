c5fb17ea007900192c4813c8df9c30fb21c1e458
Juan A. Ruz <juanantonioruz@gmail.com>
2020-10-23 13:39:28 +0200

    Updated entrypoint /lumen-dataset/<lumen_instance>/<dataset_id>
    to test it locally
    
    cd akvo-data-science-services/projects/example-lumen
    
    docker-compose up --build
    
    check =>
    http://localhost:7001/lumen-dataset/lumen/5f919b99-3cd0-4df4-a831-187e14989be7

--- projects/example-lumen/api.R
+++ projects/example-lumen/api.R
@@ -1,5 +1,7 @@
 library(here)
 library(httr)
+library(jsonlite)
+library(readr)
 
 #* Function to authenticate with auth0 using default data-science auth0 account 
 auth_headers <- function(){
@@ -10,20 +12,15 @@ auth_headers <- function(){
 #* @serializer contentType list(type="text/plain")
 #* @get /lumen-dataset/<lumen_instance>/<dataset_id>
 function(lumen_instance, dataset_id){
-    ## lumen_domain <- "akvotest"
-    ## flumen_url <- paste0("https://", lumen_instance, ".", lumen_domain, ".org/api/datasets/", dataset_id)
-    ## dataset <- GET(flumen_url, auth_headers())
-    ## content(dataset)$rows
-    "A,B,C
-a1,b1,10
-a1,b1,11
-a1,b2,9
-a1,b2,10
-a2,b1,12
-a2,b1,10
-a2,b2,11
-a2,b2,10
-"
+     lumen_domain <- "akvotest"
+     flumen_url <- paste0("https://", lumen_instance, ".", lumen_domain, ".org/api/datasets/", dataset_id)
+     dataset <- GET(flumen_url, auth_headers())
+     rows <- content(dataset, as="text")
+     result <- jsonlite::fromJSON(rows)
+     Data <- as.data.frame(result$rows)
+     names(Data) <- result$columns$title
+     model_data <- Data 
+     format_delim(model_data, delim=",")
 }
 
 
